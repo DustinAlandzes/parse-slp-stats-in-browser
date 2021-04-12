9c02c8141a8021ea40d252663a11f736798ac163
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2021-04-11 19:56:44 -0600

    eslint, run test and lint in ci (#2)
    * add eslint
    * run test and lint in ci

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Click me to open the prompt, and choose your Slippi folder/i);
   expect(linkElement).toBeInTheDocument();
 });
