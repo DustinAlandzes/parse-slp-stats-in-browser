fafaf3f6e7918c81c0645c7d6ee56bf9b2bb1b3f
Dustin Alandzes <dustinalandzes@posteo.net>
2021-04-17 18:51:29 -0500

    fix tests, stupid antd stuff, move avatar out of menu, wouter switch, jest-canvas-mock

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import UploadScreen from "./UploadScreen";
 import GameHistory from "./GameHistory";
 import {list_of_stock_icons} from "./stock_icons"
 // wouter
-import {Link, Route} from "wouter";
+import {Link, Route, Switch} from "wouter";
 
 // antd
 import 'antd/dist/antd.css'
@@ -25,19 +25,22 @@ function App(): JSX.Element {
                 {/* todo: select current item based on url */}
                 <img alt={"random stock icon"} src={list_of_stock_icons[getRandomInt(list_of_stock_icons.length)]}
                      style={{float: 'left', marginTop: '1.5em', marginRight: '1.5em'}}/>
+                <Avatar icon={<SmileOutlined/>} style={{float: 'right', marginTop: '1em'}}/>
+
                 <Menu mode="horizontal" defaultSelectedKeys={['1']}>
                     <Menu.Item key="1"><Link href="/">Home</Link></Menu.Item>
                     <Menu.Item key="2"><Link href="/upload">Parse Slippi Folder</Link></Menu.Item>
                     <Menu.Item key="3"><Link href="/history">Game History</Link></Menu.Item>
-                    <Avatar icon={<SmileOutlined/>} style={{float: 'right', marginTop: '1em'}}/>
                 </Menu>
             </Header>
             <Layout>
                 <Content style={{padding: '10px 50px', marginTop: 64}}>
                     <div style={{padding: 24, minHeight: 380}}>
-                        <Route path="/" component={Home}/>
-                        <Route path="/upload" component={UploadScreen}/>
-                        <Route path="/history" component={GameHistory}/>
+                        <Switch>
+                            <Route path="/" component={Home}/>
+                            <Route path="/upload" component={UploadScreen}/>
+                            <Route path="/history" component={GameHistory}/>
+                        </Switch>
                     </div>
                 </Content>
                 <Footer style={{textAlign: 'center'}}>
